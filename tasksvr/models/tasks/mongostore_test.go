5bf407b8c2d35c916b56cdb9839905a44bec143b
emarkovic <enamarkovic27@gmail.com>
2017-04-19 13:29:53 -0700

    mongostore

---
+++ tasksvr/models/tasks/mongostore_test.go
@@ -0,0 +1,44 @@
+package tasks
+
+import (
+	"testing"
+
+	"fmt"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+func TestCRUD(t *testing.T) {
+	sess, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		// stops automated tests
+		t.Fatalf("error dialing Mongo: %v", err)
+	}
+	defer sess.Close()
+
+	store := &MongoStore{
+		Session:        sess,
+		DatabaseName:   "test",
+		CollectionName: "tasks",
+	}
+
+	newTask := &NewTask{
+		Title: "Learn MongoDB",
+		Tags:  []string{"mongo", "info344"},
+	}
+	task, err := store.Insert(newTask)
+	if err != nil {
+		t.Errorf("error inserting new task: %v", err)
+	}
+	fmt.Println(task.ID)
+
+	task2, err := store.Get(task.ID)
+	if err != nil {
+		t.Errorf("error fetching task: %v", err)
+	}
+	if task2.Title != task.Title {
+		t.Errorf("task title didnt match, expected %s but got %s", task.Title, task2.Title)
+	}
+
+	sess.DB(store.DatabaseName).C(store.CollectionName).RemoveAll(nil)
+}
